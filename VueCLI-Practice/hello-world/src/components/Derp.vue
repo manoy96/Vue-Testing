7ce552769da6838b3819934532ea1d618ca74748
Manuel Espinoza <mfewcar@gmail.com>
2018-09-24 15:39:37 -0600

    App.vue created with 2 components

---
+++ VueCLI-Practice/hello-world/src/components/Derp.vue
@@ -0,0 +1,31 @@
+<template>
+     <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  name: 'Derp',
+  props: {
+    msg: String,
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+
