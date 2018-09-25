3ca7bd40fbe0224ae3f06a004c27d00934eed2f5
Manuel Espinoza <mfewcar@gmail.com>
2018-09-25 10:39:16 -0600

    created a new component Derp for App.vue

--- VueCLI-Practice/hello-world/src/components/Derp.vue
+++ VueCLI-Practice/hello-world/src/components/Derp.vue
@@ -12,20 +12,10 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+p{
+text-align: center;
+font-size: 36px;
+color: red;}
 </style>
 
 
